73f67fa0d5dc594505f18aa9db2c3368904bc3fd
Ashley Davis <ashley@codecapers.com.au>
2024-01-25 19:47:50 +1000

    Added a button to reset the chat thread.

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -50,7 +50,25 @@ export function App() {
         //
         // Save the thread id in local storage.
         //
-        localStorage.setItem("threadId", threadId.current);
+        localStorage.setItem("threadId", threadId.current!);
+    }
+
+    //
+    // Starts a new chat thread.
+    //
+    function onResetThread() {
+        threadId.current = undefined;
+        setMessages([]);
+        setMessage("");
+        setRunId(undefined);
+        scrollContainer.current!.scrollTop = 0;
+        localStorage.removeItem("threadId");
+
+        createThread()
+            .catch(err => {
+                console.error(`Failed to create message thread.`);
+                console.error(err);
+            });
     }
 
     //
@@ -189,7 +207,7 @@ export function App() {
             // Send message on enter.
             await onSendMessage();
         }
-    };
+    }
 
     return (
         <div>
@@ -240,7 +258,7 @@ export function App() {
                             strokeLinecap="round"
                             strokeLinejoin="round"
                         />
-                    </svg>                   
+                    </svg>
                 </div>
             </div>}
 
@@ -364,6 +382,15 @@ export function App() {
                     <div className="text-sm ml-3 pt-3 pr-1 text-gray-500">
                         Example: What is Ashley's skillset?
                     </div>
+                </div>
+
+                <div 
+                    className="absolute top-[10px] right-[10px] cursor-pointer"
+                    onClick={onResetThread}
+                    >
+                    <svg width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M 10 2 L 9 3 L 4 3 L 4 5 L 5 5 L 5 20 C 5 20.522222 5.1913289 21.05461 5.5683594 21.431641 C 5.9453899 21.808671 6.4777778 22 7 22 L 17 22 C 17.522222 22 18.05461 21.808671 18.431641 21.431641 C 18.808671 21.05461 19 20.522222 19 20 L 19 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 7 5 L 17 5 L 17 20 L 7 20 L 7 5 z M 9 7 L 9 18 L 11 18 L 11 7 L 9 7 z M 13 7 L 13 18 L 15 18 L 15 7 L 13 7 z"></path>
+                    </svg>
                 </div>
             </div>}
 
